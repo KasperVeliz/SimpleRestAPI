843bf8cb95a6b506aa85aabe2bdf12fe7b8072b5
Kasper <kasper.veliz@gmail.com>
2025-10-29 19:31:31 -0400

    re-hid sensitive info in queries.js

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
 const express = require('express')
-//const cors = require('cors')
+const cors = require('cors')
 const bodyParser = require('body-parser')
-//const helmet = require('helmet')
+const helmet = require('helmet')
 const db = require('./queries')
 
 const app = express()
 
 const port = 3000
 
-//app.use(cors())
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true,}))
-//app.use(helmet())
-
-/*
-app.use((req, res, next) => {
-    const error = new Error('Something went wrong')
-    next(error)
-})
-
-app.use((err, req, res, next) => {
-    console.error('Error: ', err.message)
-    res.status(500).send('Internal Server Error')
-})
-*/
+app.use(helmet())
 
 app.get('/', (request, response) => {
     response.json({info: 'Node.js, Express, and Postgres API'})
